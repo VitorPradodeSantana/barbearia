e11588a26c9cd1ab43aab4519ec736764ef35b7a
VitorPradodeSantana <vitor_shotokan@hotmail.com>
2022-10-03 23:55:52 -0300

    Atualizado a pagina principal

---
+++ script/script.js
@@ -0,0 +1,17 @@
+const menuMobile = document.querySelector('.menu-mobile')
+const navegacaoMobile = document.querySelector('#itens-menu-mobile')
+const btnAgendamento = document.querySelector('#btn-agendamento')
+
+function mostraMenuMobile() {
+  if (navegacaoMobile.style.display === 'block') {
+    navegacaoMobile.style.display = 'none'
+  } else {
+    navegacaoMobile.style.display = 'block'
+  }
+}
+
+btnAgendamento.addEventListener('click', () => {
+  location.href = 'agendamento.html'
+})
+
+menuMobile.addEventListener('click', mostraMenuMobile)
